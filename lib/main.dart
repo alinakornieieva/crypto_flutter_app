1622506f66c4a1ca54cfde2a315983be007e45cb
alinakornieieva <alinakorneeva401@gmail.com>
2024-04-22 20:52:34 +0300

    Added navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -13,6 +15,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
+        listTileTheme: const ListTileThemeData(iconColor: Colors.white),
         textTheme: const TextTheme(
             bodyLarge: TextStyle(
                 color: Colors.white, fontSize: 24, fontWeight: FontWeight.w600),
@@ -22,7 +25,10 @@ class MyApp extends StatelessWidget {
             background: Colors.black),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Crypto App'),
+      routes: {
+        '/': (context) => const MyHomePage(title: 'Crypto App'),
+        '/coin': (context) => const CryptoCoinScreen()
+      },
     );
   }
 }
@@ -46,18 +52,68 @@ class _MyHomePageState extends State<MyHomePage> {
           backgroundColor: theme.colorScheme.inversePrimary,
           title: Text(widget.title),
         ),
-        body: ListView.builder(
+        body: ListView.separated(
             itemCount: 10,
-            itemBuilder: (context, index) => ListTile(
-                  leading: Image.asset('assets/bitcoin_logo.png', width: 35),
-                  title: Text(
-                    'Bitcoin',
-                    style: theme.textTheme.bodyLarge,
-                  ),
-                  subtitle: Text(
-                    '70 000\$',
-                    style: theme.textTheme.bodySmall,
-                  ),
-                )));
+            separatorBuilder: (context, index) => const Divider(),
+            itemBuilder: (context, index) {
+              const coinName = 'Bicoin';
+              return ListTile(
+                leading: Image.asset('assets/bitcoin_logo.png', width: 35),
+                title: Text(
+                  coinName,
+                  style: theme.textTheme.bodyLarge,
+                ),
+                subtitle: Text(
+                  '70 000\$',
+                  style: theme.textTheme.bodySmall,
+                ),
+                trailing: const Icon(Icons.arrow_right_alt),
+                onTap: () {
+                  Navigator.of(context).pushNamed('/coin', arguments: coinName);
+                },
+              );
+            }));
+  }
+}
+
+class CryptoCoinScreen extends StatefulWidget {
+  const CryptoCoinScreen({super.key});
+
+  @override
+  State<CryptoCoinScreen> createState() => _CryptoCoinScreenState();
+}
+
+class _CryptoCoinScreenState extends State<CryptoCoinScreen> {
+  String? coinName;
+
+  @override
+  void didChangeDependencies() {
+    final args = ModalRoute.of(context)?.settings.arguments;
+    if (args == null) {
+      log('You must provide args!');
+      return;
+    }
+    if (args is! String) {
+      log('You must provide String args!');
+      return;
+    }
+    coinName = args;
+    setState(() {});
+    super.didChangeDependencies();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return Scaffold(
+      appBar: AppBar(
+          centerTitle: true,
+          backgroundColor: theme.colorScheme.inversePrimary,
+          title: const Text('Bitcoin')),
+      body: const Text(
+        'ghjk',
+        style: TextStyle(color: Colors.amber),
+      ),
+    );
   }
 }
